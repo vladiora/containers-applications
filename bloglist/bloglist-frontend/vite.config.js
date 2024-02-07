b20df9db5cd5db9d7b4ff423c800ac8f467d97cc
Vladana Ignjatic <vladana.ignjatic@develabs.net>
2024-02-07 14:39:19 +0100

    Create a containerized production setup of the bloglist app (21.22)

--- bloglist/bloglist-frontend/vite.config.js
+++ bloglist/bloglist-frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/",
-	preview: {
-		port: 8000,
-		strictPort: true,
-	},
 	server: {
 		host: true,
 		port: 8000, // This is the port which we will use in docker
